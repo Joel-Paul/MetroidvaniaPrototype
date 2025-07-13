f66a15283616f7945423546a6a578959f72846b5
Joel Paul <joel.v.paul@outlook.com>
2025-07-13 22:18:45 +1000

    Add restart button

--- src/world/world.gd
+++ src/world/world.gd
@@ -3,3 +3,5 @@ extends Node2D
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
