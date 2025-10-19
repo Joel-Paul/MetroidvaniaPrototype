6fc57accb63ce232efa2ac1bca02f05b837418d1
Joel Paul <joel.v.paul@outlook.com>
2025-10-19 21:57:40 +1100

    Implement combo reset

--- src/character_body2d/player/states/combo_state.gd
+++ src/character_body2d/player/states/combo_state.gd
@@ -2,8 +2,14 @@ extends MovementState
 
 @export var parent: StateMachine
 @export var action: Action
+@export_range(0, 1, 0.1, "or_greater", "suffix:s") var reset_time: float = 0
 
 var index: int = 0
+
+@onready var timer: Timer = Global.create_timer(self)
+
+func _ready() -> void:
+	timer.timeout.connect(_timeout)
 
 func priority() -> float:
 	if action.is_active():
@@ -11,6 +17,7 @@ func priority() -> float:
 	return 0
 
 func enter(_prev: State) -> void:
+	timer.start(reset_time)
 	parent.dirty = true
 	action.intercept()
 
@@ -18,3 +25,7 @@ func get_next() -> State:
 	var curr: State = self.next_states[index]
 	index = (index + 1) % self.next_states.size()
 	return curr
+
+func _timeout() -> void:
+	index = 0
+	print("TA")
