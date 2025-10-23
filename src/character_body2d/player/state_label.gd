e365ee7afd76c458469fe63156081af9cb1d34b4
Joel Paul <joel.v.paul@outlook.com>
2025-10-23 21:42:35 +1100

    Connect state machine signal to label via code

--- src/character_body2d/player/state_label.gd
+++ src/character_body2d/player/state_label.gd
@@ -1,5 +1,9 @@
 extends Label
 
+@export var state_machine: StateMachine
+
+func _ready() -> void:
+	state_machine.switched_state.connect(_on_movement_sm_switched_state)
 
 func _on_movement_sm_switched_state(_old: State, new: State) -> void:
 	text = new.name
