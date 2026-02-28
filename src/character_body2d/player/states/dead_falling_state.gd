a9a8612e5b2f8454026d40bdd9324ca9755cb8cb
Joel Paul <joel.v.paul@outlook.com>
2026-02-28 16:03:55 +1100

    Rename die to death for consistency

--- src/character_body2d/player/states/dead_falling_state.gd
+++ src/character_body2d/player/states/dead_falling_state.gd
@@ -7,4 +7,4 @@ func priority() -> float:
 	return base_priority if health.is_dead() and not character.is_on_floor() else 0.0
 
 func enter(_prev: State) -> void:
-	anim_player.play("die_falling")
+	anim_player.play("death_falling")
--- src/character_body2d/player/states/dead_ground_state.gd
+++ src/character_body2d/player/states/dead_ground_state.gd
@@ -6,4 +6,4 @@ func priority() -> float:
 	return base_priority if health.is_dead() else 0.0
 
 func enter(_prev: State) -> void:
-	anim_player.play("die_ground")
+	anim_player.play("death_ground")
